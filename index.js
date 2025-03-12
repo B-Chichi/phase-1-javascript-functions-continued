3d75b5e6d3193dd9540dfe89bce9ac5f52844197
Brian <brian2000bett@gmail.com>
2025-03-12 22:29:21 +0300

    functions-continued

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// code your solution here
+function saturdayFun(activity = "roller-skate") {
+  return `This Saturday, I want to ${activity}!`;
+}
+
+saturdayFun();
+console.log(saturdayFun());
+
+function mondayWork(task = "go to the office") {
+  return `This Monday, I will ${task}.`;
+}
+
+mondayWork();
+
+console.log(mondayWork());
+
+function wrapAdjective(visualFlair = "*") {
+  return function (adjective = "special") {
+    return `You are ${visualFlair}${adjective}${visualFlair}!`;
+  };
+}
+
